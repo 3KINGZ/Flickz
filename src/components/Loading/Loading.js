761982b7b0c413babdc290f1f0e7752130eb9a7a
3kingz <victorkenneth95@gmail.com>
2020-08-30 18:35:13 +0100

    changes

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,8 +1,12 @@
 import React from "react";
 import spinner from "../../assets/Spin-1s-200px.svg";
-
+import "./Loading.scss";
 const Loading = () => {
-  return <img src={spinner} alt="loading" />;
+  return (
+    <div className="loading">
+      <img src={spinner} alt="loading" />
+    </div>
+  );
 };
 
 export default Loading;
